5186b610f83c59e953065baf141ececcd599a49c
Anass Benabou <124387088+BENABOU48@users.noreply.github.com>
2024-11-07 18:05:31 +0100

    Update printreversealphabet.go

--- printreversealphabet.go
+++ printreversealphabet.go
@@ -1,11 +1,11 @@
 package main
 
-import (
-	"fmt"
-)
+import "github.com/01-edu/z01"
 
-func main() {
-	for i:= 'z'; i >= 'a'; i-- {
-		fmt.Printf("%c ", i)
-	}
+func main(){
+  for i:='z' ; i >='a'  ;i--{
+    
+  z01.PrintRune(i)
+  }
+    z01.PrintRune('\n')
 }
